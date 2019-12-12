4fe2f4f318d54d35a512fad4c5ee614b3b2de2ed
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-12-13 02:28:43 +0300

    app component installed mobX

---
+++ src/component/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'mobx-react';
+import Game from './Game'
+import GameModel from '../models/GameModel'
+
+const App = () => {
+
+  const mainStore = new GameModel();
+
+  return (
+      <Provider mainStore={mainStore} >
+        <Game/>
+      </Provider>
+  );
+
+}
+
+export default App;
---
+++ src/component/Game.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+
+@inject('mainStore') @observer
+class Game extends Component {
+
+}
+
+export default Game;

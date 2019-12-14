44cf432be0aeb603ebead9365e5b016017f1a6c4
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-12-14 16:01:12 +0300

    fixed flag bug

--- src/component/Cell.js
+++ src/component/Cell.js
@@ -12,40 +12,22 @@ const cellStyle = {
 @inject('mainStore') @observer
 class Cell extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            stripBg: '',
-            flag: false
-        };
-    }
-
-    handleClick = (e) => {
-        e.preventDefault();
-
-        this.setState({ flag: !this.state.flag });
-        console.log(this.state.flag);
-    }
-
-
     openCell = () => {
         this.props.mainStore.openCell(this.props.cell.row, this.props.cell.col)
+        console.log(this.props);
+    }
+    flaggedCell = (e) => {
+        e.preventDefault();
+        this.props.mainStore.flaggedCell(this.props.cell.row, this.props.cell.col)
     }
 
     render() {
-        cellStyle.background = (this.props.cell.opened && this.props.cell.isMine) ? '#aa0000' : '#ccc';
-        // cellStyle.background = this.props.cell.opened ? '#32a852' :  '#ccc'
-
-        const style = {
-            backgroundColor: this.state.flag ? '#32a890' : '#ccc',
-            width: 40,
-            height: 40,
-            display: 'block',
-        };
+        cellStyle.background = (this.props.cell.isFlagged) ? '#177a1e' : 
+                               (this.props.cell.opened && this.props.cell.isMine)  ? '#aa0000' : '#ccc';
         return (
-            <button style={style}
+            <button style={cellStyle}
                 onClick={this.openCell}
-                onContextMenu={this.handleClick}
+                onContextMenu={this.flaggedCell}
                 disabled={this.props.cell.opened || this.props.mainStore.gameStatus != CONSTANTS.GAME_STATUS_ONGOING}>
                 {this.props.cell.opened ? this.props.mainStore.getCellAdjacentMinesNumber(this.props.cell.row, this.props.cell.col) : ''}
             </button>
--- src/models/GameModel.js
+++ src/models/GameModel.js
@@ -4,7 +4,7 @@ import {action, observable, computed} from "mobx";
 export default class GameModel {
 
     @observable gameStatus = CONSTANTS.GAME_STATUS_ONGOING; //GAME_STATUS_STOP
-    @observable openedMines = 0;
+    @observable openedMines = 0; //нужно только для вывода тайьла
     @observable grid = {};
 
 
@@ -32,6 +32,7 @@ export default class GameModel {
         if (cell.opened) {
             return;
         }
+        console.log(cell);
 
         cell.opened = true;
         this.openedMines++;
@@ -42,8 +43,19 @@ export default class GameModel {
 
         this.checkIfWin();
 
-        if (this.getCellAdjacentMinesNumber(row, col) === 0)
+        if (this.getCellAdjacentMinesNumber(row, col) === 0) // только если прилегающих мин 0, мы открываем прилегающие клетки
             this.openAdjacentCells(row, col);
+    }
+
+    @action flaggedCell(row, col) {
+        let cell = this.grid[row][col];
+
+        // if(cell.opened) {
+        //     return;
+        // }
+
+        cell.isFlagged = !cell.isFlagged;
+        console.log(cell.isFlagged);
     }
 
     checkIfWin() {
@@ -53,22 +65,24 @@ export default class GameModel {
                 if (this.grid[r][c].opened) openedCells++;
             }
         }
-
+        // как только счетчик открытых клеток доходит до 20 игра закончена(тру)
         if (openedCells == ((CONSTANTS.GRID_ROWS * CONSTANTS.GRID_COLS) - this.getMaxMines())) {
             this.gameOver(true);
         }
     }
 
-    openAdjacentCells(row, col) {
-        let rowStart = (row == 0) ? 0 : row -1,
-            rowEnd = (row >= (CONSTANTS.GRID_ROWS - 1)) ? row : row + 1,
-            colStart = (col == 0) ? 0 : col -1,
-            colEnd = (col >= (CONSTANTS.GRID_COLS - 1)) ? col : col + 1;
+    openAdjacentCells(row, col) { // 1:1   => rowStart=0 rowEnd=2 colStart=0 colEnd=2
+        // опреелем диапазон прилегающих строк
+        let rowStart = (row == 0) ? 0 : row -1, // перескакивает на одну строку выше
+            rowEnd = (row >= (CONSTANTS.GRID_ROWS - 1)) ? row : row + 1, // перескакиваем на одну строку ниже
+        // опреелем диапазон прилегающих колонок
+            colStart = (col == 0) ? 0 : col -1, // перескакивае на одну колонку левее 
+            colEnd = (col >= (CONSTANTS.GRID_COLS - 1)) ? col : col + 1; // перескакивае на одну колонку правее 
 
         for (let r = rowStart; r <= rowEnd; r++) {
             for (let c = colStart; c <= colEnd; c++) {
                 if (!this.grid[r][c].opened)
-                    this.openCell(r, c);
+                    this.openCell(r, c); // но ведь она не будет опенед пока я не нажму
             }
         }
 
@@ -80,11 +94,11 @@ export default class GameModel {
             rowEnd = (row >= (CONSTANTS.GRID_ROWS - 1)) ? row : row + 1,
             colStart = (col == 0) ? 0 : col -1,
             colEnd = (col >= (CONSTANTS.GRID_COLS - 1)) ? col : col + 1;
-
+        // если в прилегающих клетках есть хоть одна мина mines != 0
         for (let r = rowStart; r <= rowEnd; r++) {
             for (let c = colStart; c <= colEnd; c++) {
                 if (this.grid[r][c].isMine) {
-                    mines++;
+                    mines++; 
                 }
             }
         }
@@ -98,30 +112,31 @@ export default class GameModel {
     }
 
     buildGrid() {
-        let maxMines = this.getMaxMines(), addedMines = 0, grid = {};
+        let maxMines = this.getMaxMines(), addedMines = 0, grid = {}; //получаем количество мин (5)
 
         for (let i=0; i < CONSTANTS.GRID_ROWS; i++) {
             grid[i] = {};
-            for (let k=0; k < CONSTANTS.GRID_ROWS; k+= 1) {
-                grid[i][k] = { isMine: false, opened: false, row: i, col: k};
+            for (let k=0; k < CONSTANTS.GRID_ROWS; k+= 1) { 
+                grid[i][k] = { isMine: false, opened: false, row: i, col: k, isFlagged: false};  //  в каждую ячейку помещаяем объект
             }
         }
 
-        for (let count = 1; count <= maxMines; count++) {
+        for (let count = 1; count <= maxMines; count++) { // цикл до количества мин
 
             let mineAdded = false;
             while (!mineAdded) {
-                let randomRow = Math.floor(Math.random() * CONSTANTS.GRID_ROWS);
-                let randomCol = Math.floor(Math.random() * CONSTANTS.GRID_COLS);
+                let randomRow = Math.floor(Math.random() * CONSTANTS.GRID_ROWS);  // записываем в переменную рандомное чисо [0:4]
+                let randomCol = Math.floor(Math.random() * CONSTANTS.GRID_COLS);  // записываем в переменную рандомное чисо [0:4]
 
                 if (grid[randomRow][randomCol].isMine == false) {
-                    grid[randomRow][randomCol].isMine = true;
+                    grid[randomRow][randomCol].isMine = true; // в рандомную клетку добавляем мину
                     mineAdded = true;
                 }
             }
         }
 
         this.setGrid(grid);
+        console.log(grid);
     }
 
 
